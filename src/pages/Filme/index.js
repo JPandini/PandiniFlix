bc955aaefab66d8c67cec13d49c6be4c7ac04fff
João Vitor Pandini <99928050+JPandini@users.noreply.github.com>
2023-07-27 12:14:45 -0300

    criacaoHome

---
+++ src/pages/Filme/index.js
@@ -0,0 +1,11 @@
+
+
+function Filme(){
+    return(
+        <div>
+            <h1>Bem vindo a pagina Filmes</h1>
+        </div>
+    )
+}
+
+export default Filme;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+
+
+function Home(){
+    return(
+        <div>
+            <h1>Bem vindo à Home</h1>
+        </div>
+
+    )
+}
+
+export default Home;
