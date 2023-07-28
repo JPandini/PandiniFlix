20962c72049b9531fe7b1ebe753089a45e1556fc
João Vitor Pandini <99928050+JPandini@users.noreply.github.com>
2023-07-28 11:01:24 -0300

    error-page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,6 +8,8 @@ import './home.css';
 function Home(){
  
     const [filmes, setFilmes] = useState([]);
+    const [loading, setLoading] = useState(true);
+     
 
     useEffect(() => {
         async function loadFilmes(){
@@ -20,9 +22,21 @@ function Home(){
         })
         //console.log(response.data.results.slice(0,10));
         setFilmes(response.data.results.slice(0,10));
+        setLoading(false)
+
         }
-        loadFilmes();
+            loadFilmes();
     }, [])
+
+
+    if(loading){
+    return(
+        <div className="loading">
+            <h1>Carregando Filmes....</h1>
+        </div>
+      )
+    }
+
 
     return(
         <div className="container">
