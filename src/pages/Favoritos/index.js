0b4ec8366089ecc90d5a04b96a6b695dc15977f0
João Vitor Pandini <99928050+JPandini@users.noreply.github.com>
2023-08-02 23:08:37 -0300

    favoritosPage

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,9 +1,34 @@
-
+import './favoritos.css'
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Favoritos(){
+     const [filmes, setFilmes] = useState([])
+
+    useEffect(() => {
+        const minhaLista = localStorage.getItem("@pandiniFlix");
+        setFilmes(JSON.parse(minhaLista) || [])
+
+    }, [])
+
     return(
-        <div>
-            <h1>Tela de favoritos</h1>
+        <div className='meus-filmes'>
+            <h1>MEUS FILMES</h1>
+
+        <ul>
+            {filmes.map((item)=>{
+                return(
+                    <li key={item.id}>
+                        <span>{item.title}</span>
+                        <div>
+                            <Link to={`/filme/${item.id}`}>Ver detalher</Link>
+                            <button>Excluir</button>
+                        </div>
+                    </li>
+                )
+            })}
+        </ul>
+
         </div>
     )
 }
